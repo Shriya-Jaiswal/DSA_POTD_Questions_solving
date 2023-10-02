b0e02ad75ce71dc06c0025386801034d345d1eeb
shriya jaiswal <shriyajaiswal90@gmail.com>
2023-10-02 19:15:20 +0530

    Leetcode_POTD_Approach_2

--- 11_Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color_Leetcode_2038.cpp
+++ 11_Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color_Leetcode_2038.cpp
@@ -36,10 +36,36 @@ bool winnerOfGame(string colors) {
     return false; // 👉 bob wins
 }
 
+
+// 👉 ➡️ 2nd Approach
+bool winnerOfGame2(string colors) {
+        int aliceScore = 0, bobScore = 0; // Initialize scores for Alice and Bob
+
+        // Iterate through the colors, excluding the edge pieces
+        for (int i = 1; i < colors.size() - 1; i++) {
+            char currentColor = colors[i];
+            char prevColor = colors[i - 1];
+            char nextColor = colors[i + 1];
+
+            // Check if Alice can make a move here
+            if (currentColor == 'A' && prevColor == 'A' && nextColor == 'A')
+                aliceScore++; // Alice can remove 'A'
+
+            // Check if Bob can make a move here
+            else if (currentColor == 'B' && prevColor == 'B' && nextColor == 'B')
+                bobScore++; // Bob can remove 'B'
+        }
+
+        // Determine the winner based on the scores
+        return aliceScore > bobScore;
+    }
+
+
 int main()
 {
     string colors = "AAABABB";
-    bool ans = winnerOfGame(colors);
+    // bool ans = winnerOfGame(colors);
+    bool ans = winnerOfGame2(colors);
     cout <<"ans is : "<< ans << endl;
     return 0;
 }
