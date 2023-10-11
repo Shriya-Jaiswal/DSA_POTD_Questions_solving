546cae2995b6aa3a7c061ac22e13dd2d7d32f6e0
shriya jaiswal <shriyajaiswal90@gmail.com>
2023-10-11 20:15:12 +0530

    Leetcode_POTD

---
+++ 29_Number_of_Flowers_in_Full_Bloom_Leetcode_2251.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+// ➡️ ▶️ Google
+vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
+    vector<int> start_Time;        
+    vector<int> end_Time;        
+    int n = people.size();
+
+    for(int i=0; i< flowers.size(); i++){
+        start_Time.push_back(flowers[i][0]);
+        end_Time.push_back(flowers[i][1]);
+    }
+    sort(start_Time.begin(),start_Time.end());
+    sort(end_Time.begin(),end_Time.end());
+
+    vector<int> ans(n);
+    for(int i = 0; i<n; i++){
+        int started_Blooming = upper_bound(start_Time.begin(), start_Time.end(), people[i]) - start_Time.begin();
+        int stopped_Blooming = lower_bound(end_Time.begin(), end_Time.end(), people[i]) - end_Time.begin();
+
+        ans[i] = started_Blooming - stopped_Blooming;
+    }
+    return ans;
+}
+
+
+
+int main()
+{
+    vector<vector<int>> flowers {{1,6},{3,7},{9,12},{4,13}};
+    vector<int> people {2,3,7,11};
+    
+    vector<int> ans = fullBloomFlowers(flowers,people);
+
+    for(int i =0; i<ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
