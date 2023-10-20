646bfc148901d5e98724e6d1c7b9effd892202c6
shriya jaiswal <shriyajaiswal90@gmail.com>
2023-10-20 18:49:16 +0530

    Leetcode_POTD

---
+++ 47_Flatten_Nested_List_Iterator_Leetcode_341.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+class NestedIterator {
+public:
+    queue<int> q;
+
+    void Flatten(vector<NestedInteger> &nestedList){
+        int n = nestedList.size();
+        for(int i=0; i<n; i++){
+            NestedInteger & obj = nestedList[i];
+            if(obj.isInteger()){
+                q.push(obj.getInteger());
+            }
+            else{
+                Flatten(obj.getList());
+            }
+        }
+    }
+
+
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        Flatten(nestedList);    
+    }
+    
+    int next() {
+        int num =  q.front();
+        q.pop();
+        return num;
+    }
+    
+    bool hasNext() {
+        if(q.empty()){
+            return false;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> nestedList = {{1,1},{2},{1,1}};
+    return 0;
+}
