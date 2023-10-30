2765589d3dfeef0d878322dd9f15e06a9c66c30a
shriya jaiswal <shriyajaiswal90@gmail.com>
2023-10-30 22:33:07 +0530

    Leetcode_POTD

--- 67_Sort_Integers_by_The_Number_of_1_Bits_Leetcode_1356.cpp
+++ 67_Sort_Integers_by_The_Number_of_1_Bits_Leetcode_1356.cpp
@@ -11,33 +11,39 @@ int countOneBits(int nums){
     int count = 0;
 
     while(nums){
-        count += nums & 1;
-        nums >>= 1;
+        count += nums & 1; // 👉 find last bit as a set
+        nums >>= 1; // 👉 right shift
     }
     return count;
 }
-// ⏲️ Time complexity : log2(nums)
+
+//                      👇 sorting
+// ⏲️ Time complexity : O(nlog(n)) * O(log2(nums)) 
 
 vector<int> sortByBits(vector<int>& arr) {
 
-    auto lambda =[&](int &a, int &b){
+    //           👇 [] capture clause ---> used for capture outside declared variable in comparator --> here we are taking by ref 
+    auto lambda = [&](int &a, int &b){
         int count_a = countOneBits(a);
         int count_b = countOneBits(b);
 
         if(count_a == count_b){
-            return a<b;
+            return a<b; // 👉 assending order me sort krdo
         }
 
-        return count_a < count_b;
+        return count_a < count_b; // 👉assending on basis 1 bit
     };
 
-    sort(begin(arr),end(arr),lambda);
+    sort(begin(arr),end(arr),lambda); // 👉 lambda is a comparator
 
     return arr;
 }
 
 
-// ➡️ Approach 2 ---> ⏲️ time complexity O(1)
+// ➡️ Approach 2 ---> with out write another function for count bits ---> we have a STL function 👉 __buildin_popcount() ----> jisme hum koi bhi number pass karenge toh uss number ke corresponding binary representation me 1 bit kitne hai woh bta dega  ⏲️ time complexity O(1)
+
+// ➡️ 👉 in java inbuild count set bit function is : 👉 Integer.bitcount(nums);
+
 vector<int> sortByBits_Approach2(vector<int>& arr) {
 
     auto lambda =[&](int &a, int &b){
@@ -55,6 +61,8 @@ vector<int> sortByBits_Approach2(vector<int>& arr) {
 
     return arr;
 }
+
+// :time Time complexity overall ---> O(nlogn) ---> for sorting
 
 int main()
 {
