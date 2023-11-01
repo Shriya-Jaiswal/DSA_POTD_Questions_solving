52d524f3461e2dde248137a9bbf032c3ceb44524
shriya jaiswal <shriyajaiswal90@gmail.com>
2023-11-01 19:49:56 +0530

    GFG_POTD

---
+++ 70_Frequencies_of_Limited_Range_Array_Elements_GFG.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void frequencyCount(vector<int>& arr,int N, int P)
+{ 
+    int i=0;
+    while(i<N){
+        if(arr[i]<=0 || arr[i]>N){
+            i++;
+            continue;
+        }
+        int index = arr[i]-1;
+        if(arr[index]>0){
+            arr[i] = arr[index];
+            arr[index] = -1;
+        }
+        else{
+            arr[i] = 0;
+            arr[index]--;
+            i++;
+        }
+    }
+    for(int i=0; i<N; i++){
+        if(arr[i]<0){
+            arr[i] = -1 *arr[i];
+        }
+        else{
+            arr[i] = 0;
+        }
+    }
+}
+
+// ⏲️ Time complexity : Expected time complexity: O(N)
+// 🛢️ Space Complexity : Expected auxiliay space: O(1)
+
+int main()
+{
+    int N = 5;
+    vector<int> arr{2, 3, 2, 3, 5};
+    int P = 5;
+
+    frequencyCount(arr,N,P);
+
+    for(int i=0; i<arr.size();i++){
+        cout<< arr[i] << " ";
+    }
+    cout<< endl;
+    return 0;
+}
