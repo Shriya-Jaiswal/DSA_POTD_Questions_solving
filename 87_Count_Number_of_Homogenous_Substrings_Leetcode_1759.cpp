5a0c27bd6d8f33eaa621421c4a1aaf8bae38f002
shriya jaiswal <shriyajaiswal90@gmail.com>
2023-11-09 23:28:16 +0530

    Leetcode_POTD

--- 87_Count_Number_of_Homogenous_Substrings_Leetcode_1759.cpp
+++ 87_Count_Number_of_Homogenous_Substrings_Leetcode_1759.cpp
@@ -10,7 +10,6 @@ int countHomogenous(string s) {
         int n = s.length();
         int result = 0;
         int count  = 0;
-        
         for(int i = 0; i<n; i++) {
             if(i > 0 && s[i] == s[i-1]) {
                 count++;
