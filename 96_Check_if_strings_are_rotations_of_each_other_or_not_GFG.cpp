eddc390f17c2287e9af9784aaa9d4dbf3cf61b13
shriya jaiswal <shriyajaiswal90@gmail.com>
2023-11-14 21:38:46 +0530

    GFG_POTD

---
+++ 96_Check_if_strings_are_rotations_of_each_other_or_not_GFG.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+//Function to check if two strings are rotations of each other or not.
+bool areRotations(string s1,string s2){
+    string ans=s1;
+    int n1=s1.length();
+    int n2=s2.length();
+    if(n1!=n2){
+        return false;
+    }
+    if(s1==s2){
+        return true;
+    }
+    if(s1==s2){
+        return true;
+    }
+        
+    for(int i=0;i<n1;i++){
+        char end=ans.back();
+        ans.pop_back();
+        ans.insert(ans.begin(),end);
+            
+        if(ans==s2) return true;
+    }
+    return false;
+
+    // or 
+    //string ans=s1+s1;
+    //return ans.substr(s2)!=-1;
+}
+
+int main()
+{
+    string str1 = "geeksforgeeks";
+    string str2 = "forgeeksgeeks";
+
+    bool ans = areRotations(str1, str2);
+    cout << "ans is : " << ans << endl;
+    return 0;
+}
