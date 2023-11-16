809144531fb2555c0a965a6742d8f08c30f36302
shriya jaiswal <shriyajaiswal90@gmail.com>
2023-11-16 19:11:51 +0530

    Leetcode_POTD

--- 101_Find_Unique_Binary_String_Leetcode_1980.cpp
+++ 101_Find_Unique_Binary_String_Leetcode_1980.cpp
@@ -9,16 +9,17 @@ using namespace std;
 // // ▶️ Approach-1 (Using simple conversion)
 // // ⏲️ Time Complexity : O(n^2) - Iterating on each string and converting each character to integer
 // // 🛢️ Space Complexity : O(n) - Using set
+
 // string findDifferentBinaryString(vector<string>& nums) {
 //     unordered_set<int> st;   
 //     for(string &num : nums) {
-//         st.insert(stoi(num, 0, 2));
+//         st.insert(stoi(num, 0, 2)); // 👉 convert string to number but here 2 represent binary string means ye binary number ko string me convert kr dega
 //     }
 //     int n = nums.size();
 //     string result= "";
 //     for(int number = 0; number <= 65536; number++) {
 //         if(st.find(number) == st.end()) {
-//             result = bitset<16>(number).to_string();
+//             result = bitset<16>(number).to_string(); // 👉 set the bitset of 16 length
 //             break;
 //         }
 //     }
@@ -55,7 +56,7 @@ string findDifferentBinaryString(vector<string>& nums) {
     string result;
     for(int i = 0; i<n; i++) {
         char ch = nums[i][i];  
-        result += (ch == '0') ? "1" : "0";
+        result += (ch == '0') ? "1" : "0"; // 👉 discarding nums[i]
     }
     return result;
 }
