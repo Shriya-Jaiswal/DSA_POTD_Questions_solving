5647c8ce2e3708a9e8bdbd3f36e56604c34adc31
shriya jaiswal <shriyajaiswal90@gmail.com>
2023-11-19 19:00:30 +0530

    GFG_POTD

---
+++ 106_Intersection_of_two_sorted_Linked_lists_GFG.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+using namespace std;
+
+
+// ▶️ Ask by ---> Amazon | Microsoft | D-E-Shaw | Zopper
+
+class Node{
+    public:
+        int data;
+        Node* next;
+    Node(int data){
+        this -> data = data;
+        this-> next = NULL;
+    }
+
+    ~Node(){
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 printing the Linked List
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data <<"-> ";
+        temp = temp -> next;
+    }
+    cout<< "NULL" << endl;
+}
+
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 insertAtTail
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node* &head , Node* &tail, int data){
+
+    // 👇 firstNode
+    // ➡️ check for empty Ll
+    if(head == NULL){
+        //  LL --> Node empty hogi, and first node add hogi
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // 👉 step create a node
+    Node* newNode = new Node(data);
+
+    tail -> next = newNode;
+    
+    // 👉 step 3
+    tail = newNode;
+
+}
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+    Node* ans = new Node(0);
+    Node* temp = ans;
+
+    while(head1 and head2){
+        if(head1 -> data == head2 ->data){
+            temp->next= new Node(head1->data);
+            head1 = head1 -> next;
+            head2 = head2 -> next;
+            temp = temp -> next;
+        }
+        else if(head1 -> data < head2 -> data){
+            head1 = head1 -> next;
+        }
+        else{
+            head2 = head2 -> next;
+        }
+    }
+    return ans -> next;
+}
+
+
+// ⏲️ Expected Time Complexity : O(n+m)
+// 🛢️ Expected Auxilliary Space : O(n+m)
+// ➡️ Note: n, m are the size of the respective linked lists.
+
+
+int main()
+{
+    Node* head1 = NULL;
+    Node* tail1 = NULL;
+    insertAtTail(head1,tail1,1);
+    insertAtTail(head1,tail1,2);
+    insertAtTail(head1,tail1,3);
+    insertAtTail(head1,tail1,4);
+    insertAtTail(head1,tail1,6);
+
+    Node* head2 = NULL;
+    Node* tail2 = NULL;
+    insertAtTail(head2,tail2,2);
+    insertAtTail(head2,tail2,4);
+    insertAtTail(head2,tail2,6);
+    insertAtTail(head2,tail2,8);
+
+    print(head1);
+    print(head2);
+
+    Node* ans = findIntersection(head1, head2);
+    print(ans);
+    return 0;
+}
