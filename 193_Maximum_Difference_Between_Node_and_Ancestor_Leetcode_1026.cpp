669e33e2e6d4f06facc359ae2078d24feb920997
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-01-11 18:28:41 +0530

    Maximum_Difference_Between_Node_and_Ancestor_Leetcode_1026_POTD

---
+++ 193_Maximum_Difference_Between_Node_and_Ancestor_Leetcode_1026.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+// ▶️ Ask by : ✯ Amazon  ✯ Google  ✯ Facebook   
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+    Node(int data){
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* buildTree(){
+
+    int data;
+    cout <<"Enter root data " << endl;
+    cin >> data;
+
+     if(data == -1){
+        return NULL;
+    }
+
+    // 👉 1 case solve
+    Node* root = new Node(data);
+
+    cout << "Enter data for left child of : "<< endl;
+    root -> left = buildTree();
+
+    cout << "Enter data for right child of : "<< endl;
+    root -> right = buildTree();
+
+    return root;
+}
+
+
+// ▶️ Approach-1 (Brute Force) - O(n^2) - n = number of nodes in the Tree
+/*
+  Just simply take a root, find all the differences of it from its childres and find max one
+  Again go to root->left and do the same as above
+  Again go to root->right and do the same as above
+*/
+
+int maxDiff;
+
+void findMaxUtil(Node* root, Node* child) {
+    if(!root || !child)
+        return;
+        
+    maxDiff = max(maxDiff, abs(root->data - child->data));
+
+    findMaxUtil(root, child->left);
+    findMaxUtil(root, child->right);
+}
+
+void findMaxDiff(Node* root) {
+    if(!root || !root->left && !root->right)
+        return;
+         
+        //Find max differences of this root with all its children
+        findMaxUtil(root, root->left);
+        findMaxUtil(root, root->right);
+
+        //firther move left and right
+        findMaxDiff(root->left);
+        findMaxDiff(root->right);
+}
+
+int maxAncestorDiff(Node* root) {
+    maxDiff = INT_MIN;
+
+    findMaxDiff(root);   
+
+    return maxDiff;     
+}
+
+int main()
+{
+    Node* root = NULL;
+    root = buildTree();
+
+    int ans = maxAncestorDiff(root);
+    cout << "ans is : " << ans << endl;
+
+    return 0;
+}
+
+// 8 3 1 -1 -1 6 4 -1 -1 7 -1 -1 10 -1 14 13 -1 -1 -1
