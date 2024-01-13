c94ea84ec8bbc54194054b1563f2f6ebc21e7ea8
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-01-13 21:56:03 +0530

    Insertion_Sort_for_Singly_Linked_List_GFG_POTD

---
+++ 196_Insertion_Sort_for_Singly_Linked_List_GFG.cpp
@@ -0,0 +1,119 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+    Node(int data){
+        this -> data = data;
+        this-> next = NULL;
+    }
+
+    ~Node(){
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 printing the Linked List
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data <<"-> ";
+        temp = temp -> next;
+    }
+    cout<< "NULL" << endl;
+}
+
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 insertAtTail
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node* &head , Node* &tail, int data){
+
+    // 👇 firstNode
+    // ➡️ check for empty Ll
+    if(head == NULL){
+        //  LL --> Node empty hogi, and first node add hogi
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // 👉 step create a node
+    Node* newNode = new Node(data);
+
+    tail -> next = newNode;
+    
+    // 👉 step 3
+    tail = newNode;
+
+}
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+
+Node* insertionSort(struct Node* head_ref)
+{
+    if (!head_ref || head_ref->next == NULL)
+        return head_ref;
+    Node* faltu = new Node(-1);
+    faltu->next = head_ref;
+        
+    Node * curr = head_ref->next;
+    Node * prev = head_ref;
+    while(curr != NULL){
+    Node * temp = faltu;
+    if (prev->data < curr->data){
+        curr = curr->next;
+        prev = prev->next;
+        continue;
+    }
+    while(temp->next->data < curr->data){
+        temp = temp->next;
+    }
+    prev->next = curr->next;
+    curr->next = temp->next;
+    temp->next = curr;
+    curr = prev->next;
+
+}
+    return faltu->next;
+}
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+    insertAtTail(head,tail,30);
+    insertAtTail(head,tail,23);
+    insertAtTail(head,tail,28);
+    insertAtTail(head,tail,30);
+    insertAtTail(head,tail,11);
+    insertAtTail(head,tail,14);
+    insertAtTail(head,tail,19);
+    insertAtTail(head,tail,16);
+    insertAtTail(head,tail,21);
+    insertAtTail(head,tail,25);
+    Node* ans = insertionSort(head);
+    print(ans);
+
+    return 0;
+}
