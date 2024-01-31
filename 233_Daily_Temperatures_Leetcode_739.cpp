4134be0d8c70aaef0adeb106e79718080db4a4d8
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-01-31 19:16:28 +0530

    Daily_Temperatures_Leetcode_739_POTD

---
+++ 233_Daily_Temperatures_Leetcode_739.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+// ▶️ Ask by :  Facebook ✯   Amazon ✯   Microsoft ✯   C3 IoT ✯   Uber ✯   Salesforce  
+
+// ⏲️ Time Complexity : O(n)
+// 🛢️ Space Complexity : O(n)
+
+vector<int> dailyTemperatures(vector<int>& temperatures) {
+    int n = temperatures.size();
+    stack<int> st;
+    vector<int> result(n);
+    for(int i = n-1; i>=0; i--) {
+        while(!st.empty() && temperatures[i] >= temperatures[st.top()]) {
+            st.pop();
+        }   
+        if(st.empty()) {
+            result[i] = 0;
+        } else {
+            result[i] = st.top() - i; //days
+        }       
+        st.push(i);
+    }        
+    return result;    
+}
+
+int main()
+{
+    vector<int> temperatures = {73,74,75,71,69,72,76,73};
+    vector<int> ans = dailyTemperatures(temperatures);
+    for(int i=0; i<ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
