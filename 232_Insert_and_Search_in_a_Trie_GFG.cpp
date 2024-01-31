27befb0bbea5c2f9984de87891a5ef67a56184dc
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-01-31 19:06:04 +0530

    Insert_and_Search_in_a_Trie_GFG_POTD

---
+++ 232_Insert_and_Search_in_a_Trie_GFG.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// ▶️ Ask by : Accolite | Amazon | Microsoft | D-E-Shaw | FactSet
+
+// User function template for C++
+
+// trie node
+
+struct TrieNode {
+    struct TrieNode *children[ALPHABET_SIZE];
+
+    // isLeaf is true if the node represents
+    // end of a word
+    bool isLeaf;
+};
+
+class Solution{
+    public:
+    //Function to insert string into TRIE.
+    void insert(struct TrieNode *root, string key){
+        int n=key.length();
+        TrieNode* temp=root;
+        for(int i=0;i<n;i++){
+            int ind= CHAR_TO_INDEX(key[i]);
+            if(temp->children[ind]==NULL){
+                temp->children[ind]=getNode();
+            }
+                temp=temp->children[ind];
+            }
+            temp->isLeaf=true;
+        }
+        
+//Function to use TRIE data structure and search the given string.
+bool search(struct TrieNode *root, string key) 
+{
+    int n=key.length();
+    TrieNode* temp=root;
+    for(int i=0;i<n;i++){
+        int ind= CHAR_TO_INDEX(key[i]);
+        if(temp->children[ind]==NULL){
+            return false;
+        }
+        temp=temp->children[ind];
+    }       
+    if(temp!=NULL && temp->isLeaf==true){
+        return true;
+    }
+    return false;         
+    }
+};
+
+// ⏲️ Expected Time Complexity: O(M+|key|)
+// 🛢️ Expected Auxiliary Space: O(M)
+
+int main()
+{
+    
+    return 0;
+}
