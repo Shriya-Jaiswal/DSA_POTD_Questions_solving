55aa66e48f1f66fde4fa2059f4968a736de31bec
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-02-03 20:52:57 +0530

    Decimal_Equivalent_of_Binary_Linked_List_GFG_POTD

---
+++ 238_Decimal_Equivalent_of_Binary_Linked_List_GFG.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+using namespace std;
+
+// ▶️ Ask by : Juniper Networks
+
+class Node{
+    public:
+        int data;
+        Node* next;
+    Node(int data){
+        this -> data = data;
+        this-> next = NULL;
+    }
+
+    ~Node(){
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 printing the Linked List
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data <<"-> ";
+        temp = temp -> next;
+    }
+    cout<< "NULL" << endl;
+}
+
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 insertAtTail
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node* &head , Node* &tail, int data){
+
+    // 👇 firstNode
+    // ➡️ check for empty Ll
+    if(head == NULL){
+        //  LL --> Node empty hogi, and first node add hogi
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // 👉 step create a node
+    Node* newNode = new Node(data);
+
+    tail -> next = newNode;
+    
+    // 👉 step 3
+    tail = newNode;
+
+}
+
+
+
+long long unsigned int decimalValue(Node *head)
+{
+    int n=0;
+    string str="";
+    int mod=1e9+7;
+    Node* temp=head;
+    long long unsigned int power=1;
+    long long unsigned int ans=0;
+    while(temp){
+        n++;
+        str+=char(temp->data);
+        temp=temp->next;
+    }
+    for(int i=n-1;i>=0;i--){
+        int x=int(str[i]);
+        ans= ans+x*power;
+        ans=ans%mod;
+        power= (power*2)%mod;
+    }
+    return ans%mod;
+}
+
+
+// ⏲️ Expected Time Complexity: O(n)
+// 🛢️ Expected Auxiliary Space: O(1)
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+    insertAtTail(head,tail,1);
+    insertAtTail(head,tail,1);
+    insertAtTail(head,tail,1);
+    insertAtTail(head,tail,0);
+    
+    long long unsigned int ans = decimalValue(head);
+    cout << "ans is : " << ans << endl;
+
+    return 0;
+}
