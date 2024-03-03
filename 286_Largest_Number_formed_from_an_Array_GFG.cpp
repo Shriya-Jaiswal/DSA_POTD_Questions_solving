352253e40f69b6c809e04e09dd50bb9d27f82223
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-03-03 20:46:07 +0530

    Largest_Number_formed_from_an_Array_GFG_POTD

---
+++ 286_Largest_Number_formed_from_an_Array_GFG.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+static bool compare(const string &a, const string &b) {
+    return (a + b) > (b + a);
+}
+
+
+string printLargest(int n, vector<string> &arr) {
+    sort(arr.begin(), arr.end(), compare);
+    string result;
+    for (int i = 0; i < n; ++i) {
+        result += arr[i];
+    }
+    return result;
+}
+
+int main()
+{
+    int n = 5;
+    vector<string> arr=  {"3", "30", "34", "5", "9"};
+    string ans = printLargest(n,arr);
+    cout << "ans is : " << ans << endl;
+    return 0;
+}
