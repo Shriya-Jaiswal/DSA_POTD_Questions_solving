5848e34ffdc14eddfc735f6fa82ff506860f4739
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-03-17 22:16:37 +0530

    Count_Pairs_whose_sum_is_equal_to_X_GFG_POTD

---
+++ 314_Count_Pairs_whose_sum_is_equal_to_X_GFG.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+    Node(int data){
+        this -> data = data;
+        this-> next = NULL;
+    }
+
+    ~Node(){
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 printing the Linked List
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data <<"-> ";
+        temp = temp -> next;
+    }
+    cout<< "NULL" << endl;
+}
+
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 insertAtTail
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node* &head , Node* &tail, int data){
+
+    // 👇 firstNode
+    // ➡️ check for empty Ll
+    if(head == NULL){
+        //  LL --> Node empty hogi, and first node add hogi
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // 👉 step create a node
+    Node* newNode = new Node(data);
+
+    tail -> next = newNode;
+    
+    // 👉 step 3
+    tail = newNode;
+
+}
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+// ▶️ Ask by : Amazon
+
+int countPairs(struct Node* head1, struct Node* head2, int x) {
+    set<int>st;
+    Node* temp=head1;
+    while(temp){
+        st.insert(temp->data);
+        temp=temp->next;
+    }
+        
+    int ans=0;
+    temp=head2;
+    while(temp){
+        int val=temp->data;
+        if(st.find(x-val)!=st.end()){
+            ans++;
+        }
+        temp=temp->next;
+    }
+    return ans;
+}
+
+// ⏲️ Expected Time Complexity: O(length(head1)+lenght(head2)).
+// 🛢️ Expected Auxiliary Space: O(length(head1)) or O(length(head2)).
+
+int main()
+{
+    Node* head1 = NULL;
+    Node* tail1 = NULL;
+    insertAtTail(head1,tail1,1);
+    insertAtTail(head1,tail1,2);
+    insertAtTail(head1,tail1,3);
+    insertAtTail(head1,tail1,4);
+    insertAtTail(head1,tail1,5);
+    insertAtTail(head1,tail1,6);
+    Node* head2 = NULL;
+    Node* tail2 = NULL;
+    insertAtTail(head2,tail2,11);
+    insertAtTail(head2,tail2,12);
+    insertAtTail(head2,tail2,13);
+
+    int x = 15;
+    int ans = countPairs(head1,head2,x);
+    cout<< "ans is : " << ans << endl;
+    return 0;
+}
