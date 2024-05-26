c487c74b5733403d1c7fb71ad4e0b042e18cfce8
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-05-26 21:04:49 +0530

    Student_Attendance_Record_II_Leetcode_552_POTD

---
+++ 455_Student_Attendance_Record_II_Leetcode_552.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+// // ▶️ Ask by :  Google ✯  
+
+// // ▶️ Approach-1 (Using Recursion + Memoization)
+// // ⏲️ Time Complexity : O(n) with memoization, without memoization O(3^n)
+// // 🛢️ Space Complexity : O(n)
+
+// int M = 1e9+7;
+// int t[100001][2][3];
+
+// int solve(int n, int absent, int consecutive_late) {
+
+//     if(absent >= 2 || consecutive_late >= 3) {
+//         return 0;
+//     }
+
+//     if(n == 0)
+//         return 1;
+        
+//     if(t[n][absent][consecutive_late] != -1) {
+//         return t[n][absent][consecutive_late];
+//     }
+
+//     int A = solve(n-1, absent+1, 0) % M;
+//     int L = solve(n-1, absent, consecutive_late+1) % M;
+//     int P = solve(n-1, absent, 0) % M;
+
+//     return t[n][absent][consecutive_late] = ((A + L) % M + P) % M;
+// }
+
+// int checkRecord(int n) {
+//     memset(t, -1, sizeof(t));
+//     return solve(n, 0, 0);
+// }
+
+
+
+// ▶️ Approach-2 (Using Bottom Up - Xerox of Recursion from Approach-1)
+// ⏲️ Time Complexity : O(n)
+// 🛢️ Space Complexity : O(n)
+
+int M = 1e9+7;
+
+int checkRecord(int n) {
+    int t[100001][2][3]; 
+
+    //t[i][A][L] = # award possibilities till ith day , having j absence, k consecutive lates
+
+    //Base Case - 0 number of days. So, t[0][A][L], we return 1
+    for (int A = 0; A <= 1; ++A) {
+        for (int L = 0; L <= 2; ++L) {
+            t[0][A][L] = 1;
+        }
+    }
+    for(int i = 1; i <= n; i++) {
+        for(int A = 0; A <= 1; A++) {
+            for(int L = 0; L <= 2; L++) {
+                long long result = t[i-1][A][0];         // P ---> solve(n-1, absent, 0) % M;
+                result += (L < 2 ? t[i-1][A][L+1] : 0);  // L ---> solve(n-1, absent, consecutive_late+1) % M;
+                result += (A == 0 ? t[i-1][A+1][0]: 0);  // A ---> solve(n-1, absent+1, 0) % M;
+                    
+                t[i][A][L] = result % M;
+            }
+        }
+    }
+    return t[n][0][0]; //---> return solve(n, 0, 0);
+}
+
+int main()
+{
+    int n = 2;
+    int ans = checkRecord(n);
+    cout << "ans is : " << ans << endl;
+    return 0;
+}
