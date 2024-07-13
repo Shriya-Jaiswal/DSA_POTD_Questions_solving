7e3d0201f15a76bbbcedf76f110ed16b6637fabd
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-07-13 23:05:37 +0530

    robot_Collisions_Leetcode_2751_POTD

---
+++ 544_Robot_Collisions_Leetcode_2751.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<numeric>
+#include<algorithm>
+using namespace std;
+
+// ▶️ Ask by :  Deutsche Bank ✯   Sprinklr ✯  
+
+// ▶️ Using Stack
+// ⏲️ Time Complexity : O(nlogn)
+// 🛢️ space Complexity : O(n)
+
+vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+    int n = positions.size();
+    vector<int> indices(n);
+        
+    iota(indices.begin(), indices.end(), 0); //This will fill the array as -> 0, 1, 2, 3, 4, n-1
+    stack<int> st;
+
+    auto lambda = [&](int i, int j) {
+        return positions[i] < positions[j];
+    };
+
+    sort(begin(indices), end(indices), lambda);
+
+    vector<int> result;
+    for (int currentIndex : indices) {
+        if (directions[currentIndex] == 'R') {
+            st.push(currentIndex);
+        } else {
+            while (!st.empty() && healths[currentIndex] > 0) {
+                int topIndex = st.top();
+                st.pop();
+
+                if (healths[topIndex] > healths[currentIndex]) {
+                    healths[topIndex] -= 1;
+                    healths[currentIndex] = 0;
+                    st.push(topIndex);
+                } else if (healths[topIndex] < healths[currentIndex]) {
+                    healths[currentIndex] -= 1;
+                    healths[topIndex] = 0;
+                } else {
+                    healths[currentIndex] = 0;
+                    healths[topIndex] = 0;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if (healths[i] > 0) {
+            result.push_back(healths[i]);
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    vector<int> positions = {5,4,3,2,1};
+    vector<int> healths = {2,17,9,15,10};
+    string directions = "RRRRR";
+    vector<int> ans= survivedRobotsHealths(positions,healths,directions);
+    for(int i = 0; i<ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
