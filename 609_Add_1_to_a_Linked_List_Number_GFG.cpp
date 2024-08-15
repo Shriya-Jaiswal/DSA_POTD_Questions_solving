c7ada14eebdabd93dab42a26d7115be370d7ceef
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-08-15 21:12:45 +0530

    Add_1_to_a_Linked_List_Number_GFG_POTD

---
+++ 609_Add_1_to_a_Linked_List_Number_GFG.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Node{
+    public:
+        int data;
+        Node* next;
+    Node(int data){
+        this -> data = data;
+        this-> next = NULL;
+    }
+
+    ~Node(){
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 printing the Linked List
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data <<"-> ";
+        temp = temp -> next;
+    }
+    cout<< "NULL" << endl;
+}
+
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 insertAtTail
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node* &head , Node* &tail, int data){
+
+    // 👇 firstNode
+    // ➡️ check for empty Ll
+    if(head == NULL){
+        //  LL --> Node empty hogi, and first node add hogi
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // 👉 step create a node
+    Node* newNode = new Node(data);
+
+    tail -> next = newNode;
+    
+    // 👉 step 3
+    tail = newNode;
+
+}
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+// ▶️ Ask by : Amazon
+
+Node* addOne(Node* head) {
+    // 👉 return head of list after adding one
+    Node *curr = head, *nxt = head->next, *prev = NULL;
+    while(curr){
+        nxt = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nxt;
+    }
+    Node *lastHead = prev;
+    while(prev){
+        if(prev->data!=9){
+            prev->data+=1;
+            break;
+        }
+        else prev->data=0;
+        prev=prev->next;
+    }
+    if(!prev)head->next = new Node(1);
+    curr = lastHead;
+    nxt = lastHead->next;
+    prev = NULL;
+    while(curr){
+        nxt = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nxt;
+    }
+    return prev;
+}
+
+// ⏲️ Expected Time Complexity: O(n)
+// 🛢️ Expected Auxiliary Space: O(1)
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insertAtTail(head,tail,4);
+    insertAtTail(head,tail,5);
+    insertAtTail(head,tail,6);
+
+    Node* ans = addOne(head);
+    print(ans);
+
+    return 0;
+}
