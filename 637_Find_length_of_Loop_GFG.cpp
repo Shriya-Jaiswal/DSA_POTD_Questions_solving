544b351ec2b04ce53e85359131cbc490fc07869e
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-08-29 23:02:33 +0530

    Find_length_of_Loop_GFG_POTD

---
+++ 637_Find_length_of_Loop_GFG.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+    Node(int data){
+        this -> data = data;
+        this-> next = NULL;
+    }
+
+    ~Node(){
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 printing the Linked List
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data <<"-> ";
+        temp = temp -> next;
+    }
+    cout<< "NULL" << endl;
+}
+
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 insertAtTail
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node* &head , Node* &tail, int data){
+
+    // 👇 firstNode
+    // ➡️ check for empty Ll
+    if(head == NULL){
+        //  LL --> Node empty hogi, and first node add hogi
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // 👉 step create a node
+    Node* newNode = new Node(data);
+
+    tail -> next = newNode;
+    
+    // 👉 step 3
+    tail = newNode;
+
+}
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// ▶️ Ask by : Amazon | Adobe | Qualcomm
+
+int countNodesinLoop(struct Node *head) {
+    Node *slow=head, *fast = head->next;
+    while(fast and fast->next){
+        slow=slow->next;
+        fast = fast->next->next;
+        if(slow==fast)break;
+    }
+    if(!fast or !fast->next)return 0;
+    int ans=0;
+    Node *temp = slow->next;
+    while(temp!=slow){
+        temp=temp->next;
+        ans++;
+    }
+    return ans+1;
+}
+
+// ⏲️ Expected Time Complexity: O(n)
+// 🛢️ Expected Auxiliary Space: O(1)
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insertAtTail(head,tail,1);
+    insertAtTail(head,tail,2);
+    insertAtTail(head,tail,3);
+    insertAtTail(head,tail,4);
+    insertAtTail(head,tail,5);
+
+    print(head);
+
+    int ans = countNodesinLoop(head);
+    cout << "ans is : " << ans << endl;
+
+    return 0;
+}
