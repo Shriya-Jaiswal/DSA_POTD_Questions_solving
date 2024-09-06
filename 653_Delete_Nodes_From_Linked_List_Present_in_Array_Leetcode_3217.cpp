9ecd042cff0f77e5ce93f601b8d574daa44e72e9
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-09-06 22:46:47 +0530

    Delete_Nodes_From_Linked_List_Present_in_Array_Leetcode_3217_POTD

---
+++ 653_Delete_Nodes_From_Linked_List_Present_in_Array_Leetcode_3217.cpp
@@ -0,0 +1,121 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+
+class Node{
+    public:
+        int data;
+        Node* next;
+    Node(int data){
+        this -> data = data;
+        this-> next = NULL;
+    }
+
+    ~Node(){
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 printing the Linked List
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data <<"-> ";
+        temp = temp -> next;
+    }
+    cout<< "NULL" << endl;
+}
+
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 insertAtTail
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node* &head , Node* &tail, int data){
+
+    // 👇 firstNode
+    // ➡️ check for empty Ll
+    if(head == NULL){
+        //  LL --> Node empty hogi, and first node add hogi
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // 👉 step create a node
+    Node* newNode = new Node(data);
+
+    tail -> next = newNode;
+    
+    // 👉 step 3
+    tail = newNode;
+
+}
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+// ▶️ Ask by : AMAZON
+
+// ▶️ Approach (Using Simple straight forward approach)
+// ⏲️ Time Complexity : O(n)
+// 🛢️ Space Complexity : O(n)
+
+Node* modifiedList(vector<int>& nums, Node* head) {
+    unordered_set<int> st(begin(nums), end(nums));
+    Node* prev = NULL;
+    Node* curr = head;
+
+
+    while (curr != NULL && st.count(curr->data)) {
+        head = curr->next;
+        curr = head;
+    }
+
+    while (curr != NULL) {
+        int currVal = curr->data;
+        if (!st.count(currVal)) {
+            prev = curr;
+            curr = curr->next;
+        } else {
+            prev->next = curr->next;
+            curr = curr->next;
+        }
+    }
+    return head;
+}
+
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insertAtTail(head,tail,1);
+    insertAtTail(head,tail,2);
+    insertAtTail(head,tail,3);
+    insertAtTail(head,tail,4);
+    insertAtTail(head,tail,5);
+
+    vector<int> nums = {1,2,3};
+    Node* ans = modifiedList(nums,head);
+    print(ans);
+
+    return 0;
+}
