2ee51cdbd64dcac4af72d0a7ce2513a89cbb3e86
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-09-21 23:13:45 +0530

    Clone_a_linked_list_with_next_and_random_pointer_GFG_POTD

---
+++ 680_Clone_a_linked_list_with_next_and_random_pointer_GFG.cpp
@@ -0,0 +1,121 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* random;
+    Node(int data){
+        this -> data = data;
+        this-> next = NULL;
+        this-> random = NULL;
+    }
+
+    ~Node(){
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 printing the Linked List
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data <<"-> ";
+        temp = temp -> next;
+    }
+    cout<< "NULL" << endl;
+}
+
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 insertAtTail
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node* &head , Node* &tail, int data){
+
+    // 👇 firstNode
+    // ➡️ check for empty Ll
+    if(head == NULL){
+        //  LL --> Node empty hogi, and first node add hogi
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // 👉 step create a node
+    Node* newNode = new Node(data);
+
+    tail -> next = newNode;
+    
+    // 👉 step 3
+    tail = newNode;
+
+}
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+// ▶️ Ask by : Flipkart | Morgan Stanley | Amazon | Microsoft | OYO Rooms | Snapdeal | D-E-Shaw | MakeMyTrip | Ola Cabs | Walmart | Adobe | BankBazaar
+
+Node *copyList(Node *head) {
+    Node *temp = head;
+    while(temp){
+        Node *cloneNode = new Node(temp->data);
+        cloneNode->next=temp->next;
+        temp->next=cloneNode;
+        temp=cloneNode->next;
+    }
+    temp=head;
+    while(temp){
+        if(temp->random){
+            temp->next->random=temp->random->next;
+        }
+        temp=temp->next->next;
+    }
+    Node *clone = new Node(0), *ans = clone;
+    temp=head;
+    while(temp){
+        clone->next=temp->next;
+        clone=clone->next;
+        temp->next=clone->next;
+        temp=temp->next;
+    }
+    return ans->next;
+}
+
+
+// ⏲️ Expected Time Complexity: O(n)
+// 🛢️ Expected Auxilliary Space: O(1)
+
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insertAtTail(head,tail,1);
+    insertAtTail(head,tail,2);
+    insertAtTail(head,tail,3);
+    insertAtTail(head,tail,4);
+
+    Node* ans = copyList(head);
+    print(ans);
+    return 0;
+}
