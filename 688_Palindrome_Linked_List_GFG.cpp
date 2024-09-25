2dd216772025b927a0e5109eb9e4d251b2e96198
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-09-25 22:49:08 +0530

    Palindrome_Linked_List_GFG_POTD

---
+++ 688_Palindrome_Linked_List_GFG.cpp
@@ -0,0 +1,117 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* random;
+    Node(int data){
+        this -> data = data;
+        this-> next = NULL;
+        this-> random = NULL;
+    }
+
+    ~Node(){
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 printing the Linked List
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data <<"-> ";
+        temp = temp -> next;
+    }
+    cout<< "NULL" << endl;
+}
+
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 insertAtTail
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node* &head , Node* &tail, int data){
+
+    // 👇 firstNode
+    // ➡️ check for empty Ll
+    if(head == NULL){
+        //  LL --> Node empty hogi, and first node add hogi
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // 👉 step create a node
+    Node* newNode = new Node(data);
+
+    tail -> next = newNode;
+    
+    // 👉 step 3
+    tail = newNode;
+
+}
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+// ▶️ Ask by : Accolite | Amazon | Microsoft | Snapdeal | MakeMy Trip | Adobe | Yodlee Infotech | KLA Tencor | Kritikal Solutions
+
+bool isPalindrome(Node *head) {
+    Node *slow=head,*fast=head->next;
+    while(fast and fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    Node *curr=slow->next,*prev=NULL,*nxt=NULL;
+    slow->next=NULL;
+    while(curr){
+        nxt=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=nxt;
+    }
+    while(prev){
+        if(prev->data!=head->data)return false;
+            prev=prev->next;
+        head=head->next;
+    }
+    return true;
+}
+
+
+
+// ⏲️ Expected Time Complexity: O(n)
+// 🛢️ Expected Auxiliary Space: O(1) 
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insertAtTail(head,tail,1);
+    insertAtTail(head,tail,2);
+    insertAtTail(head,tail,1);
+    insertAtTail(head,tail,1);
+    insertAtTail(head,tail,2);
+    insertAtTail(head,tail,1);
+    bool ans = isPalindrome(head);
+    cout << "ans is : " << ans << endl;
+    return 0;
+}
