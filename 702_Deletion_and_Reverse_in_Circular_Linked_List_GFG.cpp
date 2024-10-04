d7a741a86ae82b8fedf710bda484ebd4e0ee87f5
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-10-04 23:20:50 +0530

    Deletion_and_Reverse_in_Circular_Linked_List_GFG_POTD

---
+++ 702_Deletion_and_Reverse_in_Circular_Linked_List_GFG.cpp
@@ -0,0 +1,131 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* random;
+    Node(int data){
+        this -> data = data;
+        this-> next = NULL;
+        this-> random = NULL;
+    }
+
+    ~Node(){
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 printing the Linked List
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data <<"-> ";
+        temp = temp -> next;
+    }
+    cout<< "NULL" << endl;
+}
+
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 insertAtTail
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node* &head , Node* &tail, int data){
+
+    // 👇 firstNode
+    // ➡️ check for empty Ll
+    if(head == NULL){
+        //  LL --> Node empty hogi, and first node add hogi
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // 👉 step create a node
+    Node* newNode = new Node(data);
+
+    tail -> next = newNode;
+    
+    // 👉 step 3
+    tail = newNode;
+
+}
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+// ▶️ Ask by : Intuit
+
+Node* reverse(Node* head) {
+    Node *curr=head->next, *prev=head, *nxt=NULL;
+    while(curr!=head){
+        nxt=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=nxt;
+    }
+    head->next=prev;
+    return prev;
+}
+
+// 👉 Function to delete a node from the circular linked list
+Node* deleteNode(Node* head, int key) {
+    Node *temp = head->next, *prev=head;
+    if(head->data==key){
+        while(temp!=head){
+            prev=temp;
+            temp=temp->next;
+        }
+    }
+    else{
+        while(temp!=head and temp->data!=key){
+            prev=temp;
+            temp=temp->next;
+        }
+    }
+    if(head->data==key or temp!=head)
+    prev->next=temp->next;
+    return head->data==key?prev->next:head;
+}
+
+// ⏲️ Expected Time Complexity: O(n)
+// 🛢️ Expected Auxillary Space: O(1)
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insertAtTail(head,tail,2);
+    insertAtTail(head,tail,5);
+    insertAtTail(head,tail,7);
+    insertAtTail(head,tail,8);
+    insertAtTail(head,tail,10);
+
+    Node* ans1 = reverse(head);
+    print(ans1);
+
+    int key = 8;
+    Node* ans2 = deleteNode(head,key);
+    print(ans2);
+
+    return 0;
+}
