0ec53eaf3fbd4461fe9996e29f5b87e813cd7e27
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-10-07 23:25:17 +0530

    XOR_Linked_List_GFG_POTD

---
+++ 708_ XOR_Linked_List_GFG.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+#include<vector>
+#include<math.h>
+using namespace std;
+
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* random;
+    Node(int data){
+        this -> data = data;
+        this-> next = NULL;
+        this-> random = NULL;
+    }
+
+    ~Node(){
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 printing the Linked List
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data <<"-> ";
+        temp = temp -> next;
+    }
+    cout<< "NULL" << endl;
+}
+
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 insertAtTail
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node* &head , Node* &tail, int data){
+
+    // 👇 firstNode
+    // ➡️ check for empty Ll
+    if(head == NULL){
+        //  LL --> Node empty hogi, and first node add hogi
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // 👉 step create a node
+    Node* newNode = new Node(data);
+
+    tail -> next = newNode;
+    
+    // 👉 step 3
+    tail = newNode;
+
+}
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// ▶️ Ask by : Accolite | AmazonGE
+
+struct Node *insert(struct Node *head, int data) {
+    // Code here
+    Node *temp = new Node(data);
+    temp->npx = head;
+    if(head)head->npx = XOR(temp,head->npx);
+    return temp;
+}
+
+vector<int> getList(struct Node *head) {
+    // Code here
+    vector<int> ans;
+    Node *curr=head,*prev=NULL,*nxt=NULL;
+    while(curr){
+        ans.push_back(curr->data);
+        nxt = XOR(prev,curr->npx);
+        prev=curr;
+        curr=nxt;
+    }
+    return ans;
+}
+
+
+// ⏲️ Expected Time Complexity: O(n)
+// 🛢️ Expected Auxiliary Space: O(1)
+
+int main()
+{
+    
+    return 0;
+}
