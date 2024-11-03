5376866456eb946140d2797b7a67358ca74ea89d
Shriya-Jaiswal <shriyajaiswal90@gmail.com>
2024-11-03 23:20:23 +0530

    Is_Linked_List_Length_Even_GFg_POTD

---
+++ 741_Is_Linked_List_Length_Even_GFg.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* random;
+    Node(int data){
+        this -> data = data;
+        this-> next = NULL;
+        this-> random = NULL;
+    }
+
+    ~Node(){
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 printing the Linked List
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data <<"-> ";
+        temp = temp -> next;
+    }
+    cout<< "NULL" << endl;
+}
+
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+
+
+// 👉 insertAtTail
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node* &head , Node* &tail, int data){
+
+    // 👇 firstNode
+    // ➡️ check for empty Ll
+    if(head == NULL){
+        //  LL --> Node empty hogi, and first node add hogi
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // 👉 step create a node
+    Node* newNode = new Node(data);
+
+    tail -> next = newNode;
+    
+    // 👉 step 3
+    tail = newNode;
+
+}
+
+
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+// // ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ❗ ▶️ ▶️ ▶️ ▶️ ❗ ❗ ❗ ❗
+
+bool isLengthEven(struct Node **head) {
+        Node* temp= *head;
+        int count=0;   
+        while(temp){
+            temp=temp->next;
+            count++;
+        }    
+        return (count%2==0);    
+}
+
+
+// ⏲️ Expected Time Complexity: O(n)
+// 🛢️ Expected Auxillary Space: O(1)
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insertAtTail(head,tail,12);
+    insertAtTail(head,tail,52);
+    insertAtTail(head,tail,10);
+    insertAtTail(head,tail,47);
+    insertAtTail(head,tail,95);
+    insertAtTail(head,tail,0);
+
+    bool ans = isLengthEven(head);
+    cout << "ans is : " << ans << endl;
+
+    return 0;
+}
